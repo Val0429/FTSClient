0227c2f92a5dc703bdc8f3a986f4dda248d2c946
Val <val.liu@isapsolution.com>
2018-02-26 11:34:51 +0800

    add title support

--- TencentLibrary/Borders/MainBorder.xaml.cs
+++ TencentLibrary/Borders/MainBorder.xaml.cs
@@ -22,5 +22,14 @@ namespace TencentLibrary.Borders {
         public MainBorder() {
             InitializeComponent();
         }
+
+        public string Title {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(MainBorder), new PropertyMetadata(""));
+
     }
 }
