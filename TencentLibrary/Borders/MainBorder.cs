a7d3199b399535daec6a086ac55326dc76fb3e78
Val <val.liu@isapsolution.com>
2018-02-27 13:21:25 +0800

    finish contentcontrol. prepare custom area in MainBorder

--- TencentLibrary/Borders/MainBorder.cs
+++ TencentLibrary/Borders/MainBorder.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,27 +8,26 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TencentLibrary.Borders {
-    /// <summary>
-    /// Interaction logic for MainBorder.xaml
-    /// </summary>
-    public partial class MainBorder : UserControl {
-        public MainBorder() {
-            InitializeComponent();
+    public class MainBorder : ContentControl {
+        static MainBorder() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MainBorder), new FrameworkPropertyMetadata(typeof(MainBorder)));
         }
 
+        #region "Dependency Properties"
         public string Title {
             get { return (string)GetValue(TitleProperty); }
             set { SetValue(TitleProperty, value); }
         }
-        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TitleProperty =
             DependencyProperty.Register("Title", typeof(string), typeof(MainBorder), new PropertyMetadata(""));
 
+        #endregion "Dependency Properties"
     }
 }
