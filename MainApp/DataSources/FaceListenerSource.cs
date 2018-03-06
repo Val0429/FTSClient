23ca493b5e1526c344a77ae1db1b84c9b6ee717e
Val <val.liu@isapsolution.com>
2018-03-06 13:51:06 +0800

    make host & ip configurable

--- MainApp/DataSources/FaceListenerSource.cs
+++ MainApp/DataSources/FaceListenerSource.cs
@@ -1,21 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Threading;
+using WebSocketSharp;
 
 namespace Tencent.DataSources {
     public class FaceItem {
+        public string sourceid { get; set; }
         public string image { get; set; }
+        public double facewidth { get; set; }
+        public double faceheight { get; set; }
         public long createtime { get; set; }
     }
 
-    public class FaceListenerSource {
+    public class FaceListenerSource : DispatcherObject {
         public FaceListenerSource() {
-            Faces = new ObservableCollection<FaceItem>() {
-                new FaceItem() { image = "123", createtime = 456 }
+            Faces = new ObservableCollection<FaceItem>();
+
+            var host = string.Format("ws://{0}:{1}", ConfigurationManager.AppSettings["ip"], ConfigurationManager.AppSettings["port"]);
+            var ws = new WebSocket( string.Format("{0}/listen", host) );
+            ws.OnMessage += (sender, e) => {
+                var jsonSerializer = new JavaScriptSerializer();
+
+                var face = jsonSerializer.Deserialize<FaceItem>(e.Data);
+                this.Dispatcher.BeginInvoke(new Action(() => {
+                    Faces.Add(face);
+                }));
             };
+            ws.Connect();
+            Console.WriteLine("ws connected");
         }
 
         public ObservableCollection<FaceItem> Faces { get; private set; }
---
+++ WebSocketListener/Listener.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Timers;
+using WebSocketSharp;
+using Constants;
+using Newtonsoft.Json.Linq;
+
+namespace WebSocketListener
+{
+    public class Listener
+    {
+        public String URI { get; set; }
+        public String Query { get; set; }
+
+        private LogFile LogFile = new LogFile();
+
+        public event EventHandler<EventArgs<Boolean>> OnConnectionStateChange;
+        protected void RaiseOnConnectionStateChange(Boolean status)
+        {
+            if (OnConnectionStateChange != null)
+                OnConnectionStateChange(this, new EventArgs<Boolean>(status));
+        }
+
+        public event EventHandler<EventArgs<String>> OnMessageReceive;
+        protected void RaiseOnMessageReceive(String Message)
+        {
+            if (OnMessageReceive != null)
+                OnMessageReceive(this, new EventArgs<String>(Message));
+        }
+
+        public event EventHandler<EventArgs<String>> OnDropMessage;
+        protected void RaiseOnDropMessage(String Message)
+        {
+            if (OnDropMessage != null)
+                OnDropMessage(this, new EventArgs<String>(Message));
+        }
+
+        public Listener() { }
+        public Listener(String uri, String query)
+        {
+            URI = uri;
+            Query = query;
+        }
+
+        private Timer _timer = new Timer();
+        private WebSocket _listenerClient;
+        public Boolean Start()
+        {
+
+            if (String.IsNullOrEmpty(URI) || String.IsNullOrEmpty(Query))
+                return false;
+
+            _listenerClient = new WebSocket(URI);
+
+            _listenerClient.OnMessage -= _listenerClient_OnMessage;
+            _listenerClient.OnMessage += _listenerClient_OnMessage;
+
+            _listenerClient.OnError -= _listenerClient_OnError;
+            _listenerClient.OnError += _listenerClient_OnError;
+
+            _listenerClient.OnClose -= _listenerClient_OnClose;
+            _listenerClient.OnClose += _listenerClient_OnClose;
+
+            _listenerClient.Connect();
+
+            LogFile.WriteToLogFile("_listenerClient IsAlive " + (_listenerClient.IsAlive ? "true" : "false"));
+
+            if (_listenerClient.IsAlive)
+            {
+                LogFile.WriteToLogFile("_listenerClient Query " + Query);
+
+                RaiseOnConnectionStateChange(_listenerClient.IsAlive);
+
+                _listenerClient.Send(Query);
+
+                _timer.Interval = 1000;
+                _timer.Elapsed += _timer_Elapsed;
+
+                _timer.Enabled = false;
+                _timer.Enabled = true;
+                return true;
+            }
+            return false;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _timer.Enabled = false;
+            if (!_listenerClient.IsAlive)
+            {
+                _listenerClient.Close();
+                _listenerClient = null;
+
+                Start();
+            }
+            _timer.Enabled = true;
+        }
+
+        private void _listenerClient_OnClose(object sender, CloseEventArgs e)
+        {
+            LogFile.WriteToLogFile("_listenerClient OnClose " + e.Reason);
+
+            RaiseOnConnectionStateChange(_listenerClient.IsAlive);
+        }
+
+        private void _listenerClient_OnError(object sender, ErrorEventArgs e)
+        {
+            LogFile.WriteToLogFile("_listenerClient OnError " + e.Exception);
+
+            RaiseOnConnectionStateChange(_listenerClient.IsAlive);
+        }
+
+        private void _listenerClient_OnMessage(object sender, MessageEventArgs e)
+        {
+            LogFile.WriteToLogFile(e.Data);
+
+            if((e.Data.IndexOf("start") >= 0) || (e.Data.IndexOf("error") >= 0) || (e.Data.IndexOf("stop") >= 0))
+            { 
+                return;
+            }
+
+            JObject _jsonObj = JObject.Parse(e.Data);
+
+            var w = Convert.ToDouble(_jsonObj["facewidth"].ToString());
+            var h = Convert.ToDouble(_jsonObj["faceheight"].ToString());
+
+            if ( (w >=10) || (h >= 10))
+                RaiseOnMessageReceive(e.Data);
+            else
+                RaiseOnDropMessage(e.Data);
+        }
+
+        public void Stop()
+        {
+            OnMessageReceive = null; 
+            _listenerClient.OnMessage -= _listenerClient_OnMessage;
+            _listenerClient.Close();
+            _listenerClient = null;
+        }
+    }
+}
