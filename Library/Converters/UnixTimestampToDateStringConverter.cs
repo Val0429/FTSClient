1b1a083fe002efdcfe989cb77ba8f90d7d68049a
Val <val.liu@isapsolution.com>
2018-03-06 17:06:21 +0800

    add into last images, face background, scroll to bottom

---
+++ Library/Converters/UnixTimestampToDateStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Library.Converters {
+    public class UnixTimestampToDateStringConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            long unixtimestamp = (long)value;
+            System.DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dt = dt.AddMilliseconds(unixtimestamp).ToLocalTime();
+            return string.Format("{0}:{0}:{0}", dt.Hour, dt.Minute, dt.Second);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MainApp/DataSources/FaceListenerSource.cs
+++ MainApp/DataSources/FaceListenerSource.cs
@@ -15,6 +15,7 @@ namespace Tencent.DataSources {
         public string image { get; set; }
         public double facewidth { get; set; }
         public double faceheight { get; set; }
+        public double quality { get; set; }
         public long createtime { get; set; }
     }
 
@@ -23,6 +24,19 @@ namespace Tencent.DataSources {
             Faces = new ObservableCollection<FaceItem>();
 
             var host = string.Format("ws://{0}:{1}", ConfigurationManager.AppSettings["ip"], ConfigurationManager.AppSettings["port"]);
+            /// Fetch Latest
+            var wsl = new WebSocket( string.Format("{0}/latestImages", host) );
+            wsl.OnMessage += (sender, e) => {
+                var jsonSerializer = new JavaScriptSerializer();
+
+                var face = jsonSerializer.Deserialize<FaceItem>(e.Data);
+                this.Dispatcher.BeginInvoke(new Action(() => {
+                    Faces.Add(face);
+                }));
+            };
+            wsl.ConnectAsync();
+
+            /// Start Server
             var ws = new WebSocket( string.Format("{0}/listen", host) );
             ws.OnMessage += (sender, e) => {
                 var jsonSerializer = new JavaScriptSerializer();
@@ -33,15 +47,19 @@ namespace Tencent.DataSources {
                 }));
             };
             ws.OnClose += (sender, e) => {
-                ws.Connect();
+                Console.WriteLine("Close reason {0} {1}", e.Code, e.Reason);
+                ws.ConnectAsync();
                 Console.WriteLine("Do reconnect");
             };
             ws.OnError += (sender, e) => {
-                ws.Connect();
+                Console.WriteLine("Error reason {0}", e.Message);
+                ws.ConnectAsync();
                 Console.WriteLine("Do reconnect");
             };
-            ws.Connect();
-            Console.WriteLine("ws connected");
+            ws.OnOpen += (sender, e) => {
+                Console.WriteLine("connected. face listening");
+            };
+            ws.ConnectAsync();
         }
 
         public ObservableCollection<FaceItem> Faces { get; private set; }
