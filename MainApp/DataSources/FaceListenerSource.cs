15b2722f7704591faafabbe99c61ec1fe41886b3
Val <val.liu@isapsolution.com>
2018-03-06 09:56:53 +0800

    before face listener test

---
+++ MainApp/DataSources/FaceListenerSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tencent.DataSources {
+    public class FaceItem {
+        public string image { get; set; }
+        public long createtime { get; set; }
+    }
+
+    public class FaceListenerSource {
+        public FaceListenerSource() {
+            Faces = new ObservableCollection<FaceItem>() {
+                new FaceItem() { image = "123", createtime = 456 }
+            };
+        }
+
+        public ObservableCollection<FaceItem> Faces { get; private set; }
+    }
+}
