0936d7dad5bb6cef235bdb90cc59375f44d52ad8
Val <val.liu@isapsolution.com>
2018-03-08 13:16:51 +0800

    stable

---
+++ Library/Converters/ImageConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Library.Converters {
+    public class ImageConverter : IValueConverter {
+        public object Convert(
+            object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value == null) return null;
+            var bi = new BitmapImage(new Uri(value.ToString())) {
+                CacheOption = BitmapCacheOption.OnLoad
+            };
+            return bi;
+        }
+
+        public object ConvertBack(
+            object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotSupportedException();
+        }
+    }
+}
