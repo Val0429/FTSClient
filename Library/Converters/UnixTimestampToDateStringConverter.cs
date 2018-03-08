3733b0cbe4561a29cb7ad53df7d98c82a09b7745
Val <val.liu@isapsolution.com>
2018-03-08 10:44:45 +0800

    done entry time

--- Library/Converters/UnixTimestampToDateStringConverter.cs
+++ Library/Converters/UnixTimestampToDateStringConverter.cs
@@ -10,6 +10,7 @@ namespace Library.Converters {
     public class UnixTimestampToDateStringConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             long unixtimestamp = (long)value;
+            if (unixtimestamp == 0) return null;
             System.DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
             dt = dt.AddMilliseconds(unixtimestamp).ToLocalTime();
             return string.Format("{0:00}:{1:00}:{2:00}", dt.Hour, dt.Minute, dt.Second);
