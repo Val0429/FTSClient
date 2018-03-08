37b82753e7c8ca2bc8c846d1ab206270104d4be4
Val <val.liu@isapsolution.com>
2018-03-08 10:33:57 +0800

    prepare binding RT

--- MainApp/DataSources/FaceListenerSource.cs
+++ MainApp/DataSources/FaceListenerSource.cs
@@ -44,9 +44,28 @@ namespace Tencent.DataSources {
         public double score { get; set; }
     }
 
+    public class Camera {
+        public string name { get; set; }
+        public string sourceid { get; set; }
+        public int type { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+
+    public class TraceItem {
+        public TraceItem() {
+            Faces = new ObservableCollection<SearchItem>();
+        }
+        public Camera Camera { get; set; }
+        public long starttime { get; set; }
+        public long endtime { get; set; }
+        public ObservableCollection<SearchItem> Faces { get; private set; }
+    }
+
     public class FaceDetail : DependencyObject {
         public FaceDetail() {
             PossibleContacts = new ObservableCollection<SearchItem>();
+            Traces = new ObservableCollection<TraceItem>();
         }
         #region "Dependency Properties"
         public FaceItem CurrentFace {
@@ -56,7 +75,17 @@ namespace Tencent.DataSources {
         // Using a DependencyProperty as the backing store for currentFace.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CurrentFaceProperty =
             DependencyProperty.Register("CurrentFace", typeof(FaceItem), typeof(FaceDetail), new PropertyMetadata(null));
+
+        public long EntryTime {
+            get { return (long)GetValue(EntryTimeProperty); }
+            set { SetValue(EntryTimeProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for EntryTime.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EntryTimeProperty =
+            DependencyProperty.Register("EntryTime", typeof(long), typeof(FaceDetail), new PropertyMetadata(null));
         #endregion "Dependency Properties"
+
+        public ObservableCollection<TraceItem> Traces { get; private set; }
 
         public ObservableCollection<SearchItem> PossibleContacts { get; private set; }
     }
@@ -67,7 +96,24 @@ namespace Tencent.DataSources {
         public FaceListenerSource() {
             Faces = new ObservableCollection<FaceItem>();
             FaceDetail = new FaceDetail();
+            Cameras = new Dictionary<string, Camera>() {
+                ["Camera01"] = new Camera() { name = "1号摄像头大堂", sourceid = "Camera01", type = 0, X = 0, Y = 0 },
+                ["Camera02"] = new Camera() { name = "2号摄像头走廊", sourceid = "Camera02", type = 0, X = 0, Y = 0 },
+                ["Camera03"] = new Camera() { name = "3号摄像头3705会议室", sourceid = "Camera03", type = 0, X = 0, Y = 0 },
+                ["Camera04"] = new Camera() { name = "4号摄像头", sourceid = "Camera04", type = 0, X = 0, Y = 0 },
+                ["Camera05"] = new Camera() { name = "5号摄像头", sourceid = "Camera05", type = 0, X = 0, Y = 0 },
+            };
 
+            StartServer();
+        }
+
+        public ObservableCollection<FaceItem> Faces { get; private set; }
+
+        public FaceDetail FaceDetail { get; private set; }
+
+        public Dictionary<string, Camera> Cameras { get; private set; }
+
+        public void StartServer() {
             string ip, port;
             if (LicenseManager.UsageMode == LicenseUsageMode.Runtime) {
                 ip = ConfigurationManager.AppSettings["ip"];
@@ -83,7 +129,7 @@ namespace Tencent.DataSources {
 
             Host = string.Format("ws://{0}:{1}", ip, port);
             /// Fetch Latest
-            var wsl = new WebSocket( string.Format("{0}/latestImages", Host) );
+            var wsl = new WebSocket(string.Format("{0}/latestImages", Host));
             wsl.OnMessage += (sender, e) => {
                 var jsonSerializer = new JavaScriptSerializer();
 
@@ -95,7 +141,7 @@ namespace Tencent.DataSources {
             wsl.ConnectAsync();
 
             /// Start Server
-            var ws = new WebSocket( string.Format("{0}/listen", Host) );
+            var ws = new WebSocket(string.Format("{0}/listen", Host));
             ws.OnMessage += (sender, e) => {
                 var jsonSerializer = new JavaScriptSerializer();
 
@@ -120,12 +166,10 @@ namespace Tencent.DataSources {
             ws.ConnectAsync();
         }
 
-        public ObservableCollection<FaceItem> Faces { get; private set; }
-
-        public FaceDetail FaceDetail { get; private set; }
-
         public void StartSearch(FaceItem face) {
             this.FaceDetail.CurrentFace = face;
+            this.FaceDetail.EntryTime = 0;
+            this.FaceDetail.Traces.Clear();
             this.FaceDetail.PossibleContacts.Clear();
 
             var ws = new WebSocket( string.Format("{0}/search", Host) );
@@ -138,23 +182,63 @@ namespace Tencent.DataSources {
                         ws.CloseAsync();
                     }
                 } else {
-                    this.FaceDetail.Dispatcher.BeginInvoke(
-                            new Action(() => {
-                                this.FaceDetail.PossibleContacts.Add(obj_item);
-                            })
-                        );
+                    const double rate = 0.7;
+
+                        this.FaceDetail.Dispatcher.BeginInvoke(
+                                new Action(() => {
+                                    if (obj_item.score < rate) {
+
+                                        Console.WriteLine("not match! {0}", obj_item);
+                                        /// not match
+                                        this.FaceDetail.PossibleContacts.Add(obj_item);
+                                    } else {
+                                        /// matches
+                                        /// 1) get last traces, if camera match, add into it.
+                                        /// 2) if not match, add new trace, then add into it.
+
+                                        Console.WriteLine("match! {0}", obj_item);
+                                        do {
+                                            // 1)
+                                            if (this.FaceDetail.Traces.Count > 0) {
+                                                var lasttrace = this.FaceDetail.Traces[this.FaceDetail.Traces.Count - 1];
+                                                if (lasttrace.Camera.sourceid == obj_item.sourceid) {
+                                                    lasttrace.Faces.Add(obj_item);
+                                                    lasttrace.starttime = Math.Min(obj_item.createtime, lasttrace.starttime);
+                                                    lasttrace.endtime = Math.Min(obj_item.createtime, lasttrace.endtime);
+                                                    this.FaceDetail.EntryTime = Math.Min(this.FaceDetail.EntryTime, obj_item.createtime);
+                                                    break;
+                                                }
+                                            }
+
+                                            // 2)
+                                            // find camera
+                                            var camera = Cameras[obj_item.sourceid];
+                                            if (camera == null) break;
+                                            /// create trace
+                                            var traceitem = new TraceItem();
+                                            traceitem.Camera = camera;
+                                            traceitem.starttime = traceitem.endtime = obj_item.createtime;
+                                            traceitem.Faces.Add(obj_item);
+                                            this.FaceDetail.Traces.Add(traceitem);
+                                            this.FaceDetail.EntryTime = obj_item.createtime;
+
+                                        } while (false);
+                                    }
+                                })
+                            );
                 }
                 return;
             };
             ws.OnOpen += (sender, e) => {
                 var jsonSerializer = new JavaScriptSerializer();
                 var param = new SearchParam() {
-                    starttime = face.createtime - 1000000,
-                    endtime = face.createtime + 1000000,
+                    starttime = face.createtime - 1000 * 60 * 5,
+                    endtime = face.createtime + 1000 * 60 * 5,
                     image = face.image,
                     score = 0,
                     searchid = "",
                 };
+                Console.WriteLine(jsonSerializer.Serialize(param));
                 ws.SendAsync(jsonSerializer.Serialize(param), null);
             };
             ws.ConnectAsync();
