a36fe9c50cc77354666729048f914f16b5bbf290
Val <val.liu@isapsolution.com>
2018-03-10 17:05:52 +0800

    RT wrappanel performance finished!

--- Library/Extensions/ScrollViewerExtension.cs
+++ Library/Extensions/ScrollViewerExtension.cs
@@ -1,25 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Library.Extensions {
+    static class ChildHelper {
+        public static T GetChildOfType<T>(this DependencyObject depObj)
+            where T : DependencyObject {
+            if (depObj == null) return null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++) {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+
+                var result = (child as T) ?? GetChildOfType<T>(child);
+                if (result != null) return result;
+            }
+            return null;
+        }
+    }
+
     public class ScrollViewerExtension {
         public static readonly DependencyProperty AlwaysScrollToEndProperty = DependencyProperty.RegisterAttached("AlwaysScrollToEnd", typeof(bool), typeof(ScrollViewerExtension), new PropertyMetadata(false, AlwaysScrollToEndChanged));
         private static bool _autoScroll;
 
         private static void AlwaysScrollToEndChanged(object sender, DependencyPropertyChangedEventArgs e) {
-            ScrollViewer scroll = sender as ScrollViewer;
-            if (scroll != null) {
+            do {
                 bool alwaysScrollToEnd = (e.NewValue != null) && (bool)e.NewValue;
-                if (alwaysScrollToEnd) {
-                    scroll.ScrollToEnd();
-                    scroll.ScrollChanged += ScrollChanged;
-                } else { scroll.ScrollChanged -= ScrollChanged; }
-            } else { throw new InvalidOperationException("The attached AlwaysScrollToEnd property can only be applied to ScrollViewer instances."); }
+                /// try ScrollViewer
+                {
+                    ScrollViewer scroll = sender as ScrollViewer;
+                    if (scroll != null) {
+                        if (alwaysScrollToEnd) {
+                            scroll.ScrollToEnd();
+                            scroll.ScrollChanged += ScrollChanged;
+                        } else { scroll.ScrollChanged -= ScrollChanged; }
+                        return;
+                    }
+                }
+                /// try ListView
+                {
+                    ListView scroll = sender as ListView;
+                    if (scroll != null) {
+                        NotifyCollectionChangedEventHandler scrollCollectionChanged = (object s2, NotifyCollectionChangedEventArgs e2) => {
+                            if (e2.NewItems == null || scroll.Items.Count == 0) return;
+                            scroll.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.ApplicationIdle, new Action(() => {
+                                scroll.ScrollIntoView(scroll.Items[scroll.Items.Count - 1]);
+                            }));
+                        };
+
+                        if (alwaysScrollToEnd) {
+                            ((INotifyCollectionChanged)scroll.Items).CollectionChanged += scrollCollectionChanged;
+                        } else
+                            ((INotifyCollectionChanged)scroll.Items).CollectionChanged -= scrollCollectionChanged;
+                        return;
+                    }
+                }
+
+            } while (false);
+
+            throw new InvalidOperationException("The attached AlwaysScrollToEnd property can only be applied to ScrollViewer or ListView instances.");
         }
 
         public static bool GetAlwaysScrollToEnd(ScrollViewer scroll) {
@@ -42,5 +86,7 @@ namespace Library.Extensions {
             // Content scroll event : autoscroll eventually
             if (_autoScroll && e.ExtentHeightChange != 0) { scroll.ScrollToVerticalOffset(scroll.ExtentHeight); }
         }
+
     }
+
 }
