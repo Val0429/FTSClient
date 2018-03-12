22316edcbb897a524e847e5f55eea34bb8af0f70
Val <val.liu@isapsolution.com>
2018-03-12 16:51:16 +0800

    finished map content border

---
+++ MainApp/Converters/SourceIdToCameraNameConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using Tencent.DataSources;
+
+namespace Tencent.Converters {
+    public class SourceIdToCameraNameConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var cameras = (Application.Current.Resources["FaceListenerSource"] as FaceListenerSource).Cameras;
+            Camera camera = null;
+            cameras.TryGetValue((string)value, out camera);
+            return camera != null ? camera.name : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TencentLibrary/Borders/MapContentBorder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TencentLibrary.Borders {
+    public class MapContentBorder : Control {
+        static MapContentBorder() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MapContentBorder), new FrameworkPropertyMetadata(typeof(MapContentBorder)));
+        }
+
+        #region "Dependency Properties"
+        public ImageSource Image {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Image.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(ImageSource), typeof(MapContentBorder), new PropertyMetadata(null));
+
+        public string Title {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(MapContentBorder), new PropertyMetadata(null));
+
+        public string SubTitle {
+            get { return (string)GetValue(SubTitleProperty); }
+            set { SetValue(SubTitleProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SubTitleProperty =
+            DependencyProperty.Register("SubTitle", typeof(string), typeof(MapContentBorder), new PropertyMetadata(null));
+
+        #endregion "Dependency Properties"
+    }
+}
