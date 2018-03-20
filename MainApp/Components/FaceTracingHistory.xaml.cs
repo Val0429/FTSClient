a4a9354e7cc8a749026fea77de27526d883b22e3
Val <val.liu@isapsolution.com>
2018-03-20 09:54:59 +0800

    filter finished

--- MainApp/Components/FaceTracingHistory.xaml.cs
+++ MainApp/Components/FaceTracingHistory.xaml.cs
@@ -27,6 +27,22 @@ namespace Tencent.Components {
             InitializeComponent();
 
             SetValue(PanelProperty, new ObservableCollection<FaceTracingBorder>());
+
+            CollectionView view = CollectionViewSource.GetDefaultView((this.FindResource("MainContent") as ListView).ItemsSource) as CollectionView;
+            view.Filter = (object item) => {
+                FaceItem face = (FaceItem)item;
+                if (FilterName != null && FilterName.Length > 0) {
+                    if (FilterName == face.name) return true;
+                    return false;
+                }
+
+                if (FilterGroupAll == true) return true;
+                if (FilterGroupVIP == true && face.groupname == "VIP") return true;
+                if (FilterGroupBlacklist == true && face.groupname == "Blacklist") return true;
+                if (FilterGroupStranger == true && face.groupname == "Stranger") return true;
+
+                return false;
+            };
         }
 
         #region "Dependency Properties"
@@ -46,6 +62,64 @@ namespace Tencent.Components {
         // Using a DependencyProperty as the backing store for IconType.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IconTypeProperty =
             DependencyProperty.Register("IconType", typeof(int), typeof(FaceTracingHistory), new PropertyMetadata(0));
+
+        public int FilterType {
+            get { return (int)GetValue(FilterTypeProperty); }
+            set { SetValue(FilterTypeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilterType.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterTypeProperty =
+            DependencyProperty.Register("FilterType", typeof(int), typeof(FaceTracingHistory), new PropertyMetadata(0));
+
+        public string FilterName {
+            get { return (string)GetValue(FilterNameProperty); }
+            set { SetValue(FilterNameProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for FilterName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterNameProperty =
+            DependencyProperty.Register("FilterName", typeof(string), typeof(FaceTracingHistory), new PropertyMetadata("Val"));
+
+        public bool FilterGroupAll {
+            get { return (bool)GetValue(FilterGroupAllProperty); }
+            set { SetValue(FilterGroupAllProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for FilterGroupAll.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterGroupAllProperty =
+            DependencyProperty.Register("FilterGroupAll", typeof(bool), typeof(FaceTracingHistory), new PropertyMetadata(true));
+
+        public bool FilterGroupVIP {
+            get { return (bool)GetValue(FilterGroupVIPProperty); }
+            set { SetValue(FilterGroupVIPProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilterGroupVIP.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterGroupVIPProperty =
+            DependencyProperty.Register("FilterGroupVIP", typeof(bool), typeof(FaceTracingHistory), new PropertyMetadata(false));
+
+
+
+        public bool FilterGroupBlacklist {
+            get { return (bool)GetValue(FilterGroupBlacklistProperty); }
+            set { SetValue(FilterGroupBlacklistProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilterGroupBlacklist.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterGroupBlacklistProperty =
+            DependencyProperty.Register("FilterGroupBlacklist", typeof(bool), typeof(FaceTracingHistory), new PropertyMetadata(false));
+
+
+
+        public bool FilterGroupStranger {
+            get { return (bool)GetValue(FilterGroupStrangerProperty); }
+            set { SetValue(FilterGroupStrangerProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilterGroupStranger.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterGroupStrangerProperty =
+            DependencyProperty.Register("FilterGroupStranger", typeof(bool), typeof(FaceTracingHistory), new PropertyMetadata(false));
+
+
 
         #endregion "Dependency Properties"
 
@@ -75,6 +149,29 @@ namespace Tencent.Components {
         private void LBIcon_MouseDown(object sender, MouseButtonEventArgs e) {
             if (IconType == 0) IconType = 1;
             else IconType = 0;
+        }
+
+        private void FilterIcon_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (FilterType == 0) FilterType = 1;
+            else FilterType = 0;
+
+            this.ContentArea.Content = FilterType == 0 ? this.FindResource("MainContent") : this.FindResource("FilterContent");
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e) {
+            FilterType = 0;
+            this.ContentArea.Content = this.FindResource("MainContent");
+            (CollectionViewSource.GetDefaultView((this.FindResource("MainContent") as ListView).ItemsSource) as CollectionView).Refresh();
+        }
+
+        private void Filter_Group_All_Click(object sender, RoutedEventArgs e) {
+            //CheckBox cb = (CheckBox)sender;
+
+            //((this.FindResource("FilterContent") as FrameworkElement).FindName("Filter_Group_VIP") as CheckBox).IsChecked = cb.IsChecked;
+
+            //(LogicalTreeHelper.FindLogicalNode(this, "Filter_Group_VIP") as CheckBox).IsChecked = cb.IsChecked;
+            //(LogicalTreeHelper.FindLogicalNode(this, "Filter_Group_Blacklist") as CheckBox).IsChecked = cb.IsChecked;
+            //(LogicalTreeHelper.FindLogicalNode(this, "Filter_Group_Stranger") as CheckBox).IsChecked = cb.IsChecked;
         }
     }
 }
