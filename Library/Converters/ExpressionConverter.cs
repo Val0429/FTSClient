1055cf7a6aec7d4d5cadc6687ef6bfe531c84b45
Val <val.liu@isapsolution.com>
2018-03-23 16:09:54 +0800

    done expression!

--- Library/Converters/ExpressionConverter.cs
+++ Library/Converters/ExpressionConverter.cs
@@ -8,13 +8,14 @@ using System.Windows.Data;
 using System.Data;
 
 namespace Library.Converters {
-    public class CalculationConverter : IValueConverter {
+    public class ExpressionConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             string param = (string)parameter;
             double val = double.Parse(value.ToString());
-            using (DataTable dt = new DataTable()) {
-                return dt.Compute(string.Format(param, val), null);
-            }
+
+            var engine = new Jurassic.ScriptEngine();
+            var result = engine.Evaluate(string.Format(param, val));
+            return result;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
